37a21372bf5c3e367f9328e37102000acb5776ef
VonBurrito <hamza.amentag@hotmail.com>
2022-02-23 23:45:03 +0100

    2nd commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     
     public static void main(String[] args) {
-        Enterprise enterprise = new EnterpriseBuilder().setAddress("15 elm street").setDirector(new Director("Me", "he")).getEnterpris();
-
+        Enterprise enterprise = new EnterpriseBuilder().setAddress("15 elm street").setDirector(new Director("Foo", "Foo")).getEnterpris();
+        Enterprise enterprise1 = new Enterprise(1L, "Oracle", "", new Director("Larry", "Ellison"));
         System.out.println(enterprise.toString());
+        System.out.println(enterprise1.toString());
     }
 }
